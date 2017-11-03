37e4657c6ca7fc05b7068dd7486d6f6cc38cd97b
Adam H. Sparks <adamhsparks@gmail.com>
2017-11-03 13:42:07 +1000

    add "nasapower-package.R" and corresponding documentation

---
+++ R/nasapower-package.R
@@ -0,0 +1,10 @@
+
+#' Downloads NASA-POWER Agroclimatology Variables and Returns a Tidy Data Frame
+#'
+#' @name nasapower
+#' @author Adam H Sparks
+#' @docType package
+#'
+#' @seealso
+#'\link{get_nasa}
+NULL
