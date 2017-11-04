d044cbb6aec022a5d86cc119fb5ddc7c9d3a8c77
Adam H. Sparks <adamhsparks@gmail.com>
2017-11-04 12:15:13 +1000

    Update startup message
    Add quotes to quote

--- R/zzz.R
+++ R/zzz.R
@@ -14,10 +14,10 @@
                 "in anyway, we encourage users to follow the requests of the\n",
                 "POWER Project Team.",
                 "\n",
-                "When POWER data products are used in a publication, we\n",
+                "`When POWER data products are used in a publication, we\n",
                 "request the following acknowledgment be included:\n",
                 "These data were obtained from the NASA Langley Research\n",
                 "Center POWER Project funded through the NASA Earth Science\n",
-                "Directorate Applied Science Program.\n")
+                "Directorate Applied Science Program.`\n")
   packageStartupMessage(msg)
 }
