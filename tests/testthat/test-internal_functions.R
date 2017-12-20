efb711c33c06117c3a81dc32f296e77b1bc53928
Adam H. Sparks <adamhsparks@gmail.com>
2017-12-20 15:28:38 +1000

    fix .check_vars test for invalid var

--- tests/testthat/test-internal_functions.R
+++ tests/testthat/test-internal_functions.R
@@ -72,7 +72,8 @@ test_that(".create_nasa_df creates a data frame of data", {
 
 # check that user entered vars are properly validated --------------------------
 test_that(".check_vars stops if an invalid entry is made", {
-  expect_error(.check_vars(vars = "asdfasdf"))
+  vars <- "asdfasdf"
+  expect_error(.check_vars(vars))
 })
 
 test_that(".check_vars passes if a valid entry is made", {
