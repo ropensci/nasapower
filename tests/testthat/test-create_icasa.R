803b4d604198403a62e7726cceb889802b2ce516
adamhsparks <adamhsparks@gmail.com>
2018-08-04 21:19:47 +0100

    Upgrade tests for create_icasa

--- tests/testthat/test-create_icasa.R
+++ tests/testthat/test-create_icasa.R
@@ -15,7 +15,7 @@ test_that("create_icasa stops if user specifies global coverage", {
 })
 
 test_that("create_icasa stops if user fails to specify dsn", {
-  vcr::use_cassette("create_icasa_global_stop", {
+  vcr::use_cassette("create_icasa_no_file_stop", {
     expect_error(
       create_icasa(
         lonlat = "global",
@@ -27,7 +27,7 @@ test_that("create_icasa stops if user fails to specify dsn", {
 })
 
 test_that("create_icasa stops if user fails to specify file name", {
-  vcr::use_cassette("create_icasa_global_stop", {
+  vcr::use_cassette("create_icasa_no_dsn_stop", {
     expect_error(
       create_icasa(
         lonlat = "global",
@@ -36,4 +36,19 @@ test_that("create_icasa stops if user fails to specify file name", {
       ), regexp = "*You must specify a file path and name where to save.*"
     )
   })
+})
+
+test_that("create_icasa creates a txt file with proper values", {
+  vcr::use_cassette("create_icasa", {
+      create_icasa(
+        lonlat = c(151.81, -27.48),
+        dates = c("1983-01-01"),
+        file_out = "icasa.txt",
+        dsn = tempdir()
+      )
+    icasa <- readLines(file.path(tempdir(), "icasa.txt"))
+  expect_true(any(grepl("icasa.txt", list.files(tempdir()))))
+  expect_equal(length("icasa"), 15)
+    })
+
 })
