03f356cbb706d69a91a6a1b241f66975c6258b92
Adam H. Sparks <adamhsparks@gmail.com>
2018-08-30 20:48:14 +1000

    Adds test for community and pars agreement

--- tests/testthat/test-check_community.R
+++ tests/testthat/test-check_community.R
@@ -2,6 +2,7 @@
 # community checks -------------------------------------------------------------
 context("Test that check_community function handles community strings
         correctly")
+
 test_that("check_community fails if one not provided", {
   expect_error(check_community())
 })
@@ -16,3 +17,12 @@ test_that("check_community returns a character string matching a community", {
   expect_equal(check_community("SB"), "SB")
   expect_equal(check_community("SSE"), "SSE")
 })
+
+test_that(
+  "If an invalid community is given for a par, an error occurs", {
+    pars <- "ALLSKY_SFC_SW_DWN_00_GMT"
+    temporal_average <- "DAILY"
+    lonlat <- "GLOBAL"
+    community <- "AG"
+    expect_error(check_community(community, pars))
+  })
