0ccaa562e74e983e75c15c70b5a8fb2965ac9531
Adam H. Sparks <adamhsparks@gmail.com>
2018-09-12 09:03:12 +1000

    Removes startup message
    - Move startup message information into `citation()` header

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,2 @@
-
-.onAttach <- function(libname, pkgname) { # nocov start
-  msg <- paste0("While nasapower does not redistribute the data in any way,\n",
-                "we encourage users to follow the requests of the POWER\n",
-                "Project Team:\n",
-                "\n",
-                "'When POWER data products are used in a publication, we\n",
-                "request the following acknowledgment be included:\n",
-                "These data were obtained from the NASA Langley Research\n",
-                "Center POWER Project funded through the NASA Earth Science\n",
-                "Directorate Applied Science Program.'\n",
-                "\n",
-                "Please see `citation('nasapower')` for a proper citation of\n",
-                "this package.\n")
-  packageStartupMessage(msg)
-}
-
 # load parameters data frame in session
-data("parameters", envir = environment()) # nocov end
+data("parameters", envir = environment()) # nocov
