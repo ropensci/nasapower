de5e6e3aec30107a65519929f66ea21ac424b594
Adam H. Sparks <adamhsparks@gmail.com>
2018-10-16 09:25:09 +1000

    Updates test for create_met() to generate file for whole year to avoid WARNING

--- tests/testthat/test-create_met.R
+++ tests/testthat/test-create_met.R
@@ -6,7 +6,7 @@ test_that("create_met stops if user specifies global coverage", {
     expect_error(
       power_query <- create_met(
         lonlat = "global",
-        dates = c("1983-01-01"),
+        dates = "1983-01-01",
         dsn = tempdir(),
         file_out = tmpfile.txt
       ),
@@ -19,16 +19,17 @@ test_that("create_met creates a .met file for APSIM use", {
   vcr::use_cassette("create_met", {
     power_query <- create_met(
       lonlat = c(151.81, -27.48),
-      dates = c("1985-01-01", "1985-01-02"),
+      dates = c("1985-01-01", "1985-12-31"),
       dsn = tempdir(),
       file_out = "APSIM"
     )
 
     met <- readLines(file.path(tempdir(), "APSIM.met"))
     expect_true(any(grepl("APSIM.met", list.files(tempdir()))))
-    expect_equal(length(met), 14)
+    expect_equal(length(met), 377)
     expect_equal(nchar(met)[[1]], 21)
     expect_equal(nchar(met)[[14]], 28)
+    expect_equal(nchar(met)[[311]], 31)
   })
 })
 
