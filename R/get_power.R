ba53221723c44171ad3906c7ac867cb64bd64f5a
Adam H. Sparks <adamhsparks@gmail.com>
2018-10-17 10:24:49 +1000

    Updates examples for get_power()

--- R/get_power.R
+++ R/get_power.R
@@ -86,28 +86,35 @@
 #' \donttest{
 #' # Fetch daily "AG" community temperature, relative humidity and precipitation
 #' # for January 1 1985 for Kingsthorpe, Queensland, Australia
-#' daily_ag <- get_power(community = "AG",
-#'                       lonlat = c(151.81, -27.48),
-#'                       pars = c("RH2M", "T2M", "PRECTOT"),
-#'                       dates = "1985-01-01",
-#'                       temporal_average = "DAILY"
+#' ag_d <- get_power(
+#' community = "AG",
+#' lonlat = c(151.81, -27.48),
+#' pars = c("RH2M", "T2M", "PRECTOT"),
+#' dates = "1985-01-01",
+#' temporal_average = "DAILY"
 #' )
 #'
-#' # Fetch global AG climatology for temperature, relative humidity and
-#' # precipitation
-#' climatology_ag <- get_power(community = "AG",
-#'                             pars = c("RH2M", "T2M", "PRECTOT"),
-#'                             temporal_average = "CLIMATOLOGY"
-#'                             )
+#' ag_d
+#'
+#' # Fetch global AG climatology for temperature
+#' ag_c <- get_power(community = "AG",
+#' pars = "T2M",
+#' temporal_average = "CLIMATOLOGY")
+#'
+#' ag_c
 #'
 #' # Fetch interannual solar cooking parameters for a given region
-#' interannual_sse <- get_power(community = "SSE",
-#'                              lonlat = c(112.5, -55.5, 115.5, -50.5),
-#'                              dates = c("1984", "1985"),
-#'                              temporal_average = "INTERANNUAL",
-#'                              pars = c("CLRSKY_SFC_SW_DWN",
-#'                                       "ALLSKY_SFC_SW_DWN")
-#'                                       )
+#' sse_i <- get_power(
+#'   community = "SSE",
+#' lonlat = c(112.5, -55.5, 115.5, -50.5),
+#' dates = c("1984", "1985"),
+#' temporal_average = "INTERANNUAL",
+#' pars = c("CLRSKY_SFC_SW_DWN",
+#' "ALLSKY_SFC_SW_DWN")
+#' )
+#'
+#' sse_i
+#'
 #' }
 #'
 #' @author Adam H. Sparks, \email{adamhsparks@@gmail.com}
