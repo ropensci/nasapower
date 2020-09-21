2b0254381c778ba0d70906f8d19ede27f0aae209
Adam H. Sparks <adamhsparks@gmail.com>
2020-09-22 08:39:06 +1000

    Titles to sentence case

--- R/parameters_data.R
+++ R/parameters_data.R
@@ -1,5 +1,5 @@
 
-#' NASA POWER Parameters Available for Download
+#' NASA POWER parameters available for download
 #'
 #' An \R \code{list} object of \acronym{POWER} parameters and metadata available
 #' for querying from the \acronym{POWER} database.
