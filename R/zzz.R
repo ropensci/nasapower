a4940dfd6ad2bbe72f7579f2706e2d7bb40ac540
Adam Sparks <adamhsparks@icloud.com>
2021-08-01 18:34:31 +0800

    Revise data loading to use internal only data

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,8 @@
 # load parameters data frame in session
-data("parameters", envir = environment()) # nocov
+data(sysdata, envir = environment()) # nocov
 
 # create a 'base_url' object to be shared across functions. Putting it here in
 # one spot makes it easy to update when the API changes rather than spread
 # across the package in several spots
 options("nasapower_base_url" =
           "https://power.larc.nasa.gov/beta/api/system/manager/")
-
