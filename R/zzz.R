cb68ff7e62a237eb32382db5e262d3a6f5510389
Adam Sparks <adamhsparks@icloud.com>
2021-08-15 13:48:52 +0800

    Remove zzz.R, drop base_url options

--- R/zzz.R
+++
@@ -1,5 +0,0 @@
-# create a 'base_url' object to be shared across functions. Putting it here in
-# one spot makes it easy to update when the API changes rather than spread
-# across the package in several spots
-options("nasapower_base_url" =
-          "https://power.larc.nasa.gov/api/")
