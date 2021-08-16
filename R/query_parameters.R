be3ea20b3958cf4f71b30cb78c6b5f5213ea081b
Adam Sparks <adamhsparks@gmail.com>
2021-08-16 14:55:46 +0800

    Rewrite 2 functions to 1 for querying information on available parameters

--- R/query_parameters.R
+++ R/query_parameters.R
@@ -1,65 +1,29 @@
 
-#' Query the POWER API for detailed information on a given POWER parameter
+#' Query the POWER API for detailed information on parameters
 #'
 #' Queries the \acronym{POWER} \acronym{API} returning detailed information on
-#'  a single user-provided parameter, `par`.
+#'  available parameters.
 #'
-#' @param par A character vector of a single solar, meteorological or
-#'  climatology parameter to query.  If unsure check [query_parameters()] for a
-#'  full list of parameters available for each temporal \acronym{API} and
-#'  community.
-#' @return A [list] defining the given `par` and providing valid temporal
-#'  \acronym{API} endpoints and communities for the given `par`.
+#' @details If `par` is not provided all possible parameters for the provided
+#'  community, `community` and temporal \acronym{API}, `temporal_api` will be
+#'  returned.  If only a single parameter is supplied with no `community` or
+#'  `temporal_api` then the complete attribute information for that parameter
+#'  will be returned for all possible communities and temporal \acronym{API}s
+#'  combinations.  If all three values are provided, only the information for
+#'  that specific combination of parameter, temporal \acronym{API} and community
+#'  will be returned.
 #'
-#' @examplesIf interactive()
-#' query_par("T2M")
+#' @param community An optional character vector providing community name:
+#'   \dQuote{ag}, \dQuote{sb} or \dQuote{re}.
+#' @param par An optional character vector of a single solar, meteorological or
+#'  climatology parameter to query.  If unsure, omit this argument for for a
+#'  full list of all the parameters available for each temporal \acronym{API}
+#'  and community.
+#' @param temporal_api An optional character vector indicating the temporal
+#'   \acronym{API} end-point for data being queried, supported values are
+#'   \dQuote{hourly}, \dQuote{daily}, \dQuote{monthly} or \dQuote{climatology}.
 #'
-#' @author Adam H. Sparks, \email{adamhsparks@@gmail.com}
-#'
-#' @seealso query_parameters
-#'
-#' @export
-query_par <- function(par) {
-  par <- toupper(par)
-  par_def <-
-    jsonlite::fromJSON(paste0("https://power.larc.nasa.gov/api/",
-                              "system/manager/parameters?",
-                              par))
-  return(par_def)
-}
-
-
-#' Query the POWER API for parameter descriptions
-#'
-#' Queries the \acronym{POWER} \acronym{API} for detailed information for all
-#'  parameters provided by the user-provided community and temporal \acronym{API}
-#'  endpoints.
-#'
-#' @param temporal_api Temporal average for data being queried, supported
-#'   values are \dQuote{hourly}, \dQuote{daily}, \dQuote{monthly} or
-#'   \dQuote{climatology}.  See argument details for more.
-#' @param community A character vector providing community name: \dQuote{ag},
-#'   \dQuote{sb} or \dQuote{re}.  See argument details for more.
-#'
-#' @section Argument details for \dQuote{community}: there are three valid
-#'   values, one must be supplied.  This  will affect the units of the parameter
-#'   and the temporal display of time series data.
-#'
-#' \describe{
-#'   \item{ag}{Provides access to the Agroclimatology Archive, which
-#'  contains industry-friendly parameters formatted for input to crop models.}
-#'
-#'  \item{sb}{Provides access to the Sustainable Buildings Archive, which
-#'  contains industry-friendly parameters for the buildings community to include
-#'  parameters in multi-year monthly averages.}
-#'
-#'  \item{re}{Provides access to the Renewable Energy Archive, which contains
-#'  parameters specifically tailored to assist in the design of solar and wind
-#'  powered renewable energy systems.}
-#'  }
-#'
-#' @section Argument details for `temporal_api`: There are four valid
-#'  values.
+#' @section Argument details for `temporal_api`: There are four valid values.
 #'  \describe{
 #'   \item{hourly}{The hourly average of `pars` by hour, day, month and
 #'    year.}
@@ -70,35 +34,77 @@ query_par <- function(par) {
 #'    monthly average, maximum, and/or minimum values.}
 #'  }
 #'
+#' @examplesIf interactive()
+#'
+#' # fetch the complete set of attribute information for "T2M".
+#' query_parameters(par = "T2M")
+#'
+#' # fetch complete temporal and community specific attribute information
+#' # for "T2M" in the "ag" community for the "hourly" temporal API.
+#' query_parameters(par = "T2M",
+#'                  community = "ag",
+#'                  temporal_api = "hourly")
+#'
+#' # fetch complete temporal and community specific attribute information
+#' # for all parameters in the "ag" community for the "hourly" temporal API.
+#' query_parameters(community = "ag",
+#'                  temporal_api = "hourly")
+#'
 #' @author Adam H. Sparks, \email{adamhsparks@@gmail.com}
 #'
-#' @return A [list] of definitions providing valid parameters and their
-#'  definitions for each of the three \acronym{API} endpoints and three
-#'  communities
-#'
-#' @examplesIf interactive()
-#' # Get a full list of all valid parameters for the 'DAILY' API for the 'AG'
-#' # community
-#' query_parameters(temporal_api = "DAILY", community = "AG")
-#'
-#' @seealso query_par
+#' @return A [list] object of information for the requested parameter(s) (if
+#'  requested), community and temporal \acronym{API}.
 #'
 #' @export
-#'
-query_parameters <- function(temporal_api, community) {
+query_parameters <- function(community = NULL,
+                             par = NULL,
+                             temporal_api = NULL) {
+  power_url <-
+    paste0("https://power.larc.nasa.gov/api/system/manager/parameters")
 
-  temporal_api <- toupper(temporal_api)
-  community <- toupper(community)
-
-  query_hourly_ag <- function()
-
-  parameters <-
-    jsonlite::fromJSON(
-        paste0(getOption("nasapower_base_url"),
-               "system/manager/parameters?",
-        temporal_api,
-        "&community=",
-        community
+  # if only a `par` is provided, then create URL w/o using crul and parse w/
+  # jsonlite
+  if (is.null(community) && is.null(temporal_api)) {
+    return(jsonlite::fromJSON(paste0(
+      power_url, "/", par, "?user=nasapowerdev"
+    )))
+  } else {
+    if (is.null(community) || is.null(temporal_api)) {
+      stop(call. = FALSE,
+           "`commmunity` and `temporal_api` strings must be supplied.")
+    }
+    query_list <-
+      list(
+        "community" = community,
+        "parameters" = par,
+        "temporal" = temporal_api,
+        user = "nasapowerdev"
       )
-    )
+
+    # if a `par` isn't supplied, remove this from the query list
+    query_list <- query_list[lengths(query_list) != 0]
+
+    client <- crul::HttpClient$new(url = power_url)
+
+    tryCatch({
+      # nocov begin
+      response <- client$get(query = query_list, retry = 6)
+      if (!response$success()) {
+        stop(call. = FALSE)
+      }
+    },
+    error = function(e) {
+      e$message <-
+        paste(
+          "Something went wrong with the query, the requested information was
+        not returned. Please see <https://power.larc.nasa.gov> for potential
+        server issues.\n"
+        )
+      # Otherwise refers to open.connection
+      e$call <- NULL
+      stop(e)
+    }) # nocov end
+
+    return(jsonlite::fromJSON(response$parse(encoding = "UTF8")))
+  }
 }
