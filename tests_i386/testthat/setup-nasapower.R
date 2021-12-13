bc0ec3c4df09140d734c7b73dae0ff5b2d502132
Maëlle Salmon <maelle.salmon@yahoo.se>
2021-12-13 12:04:49 +0100

    Clarify possible hack

--- tests_i386/testthat/setup-nasapower.R
+++ tests_i386/testthat/setup-nasapower.R
@@ -1,3 +1,3 @@
 library("vcr") # *Required* as 'vcr' is set up on loading
-invisible(vcr::vcr_configure(dir = vcr::vcr_test_path("../fixtures")))
+invisible(vcr::vcr_configure(dir = "../fixtures"))
 vcr::check_cassette_names()
