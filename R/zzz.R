fd25221f0241d662383f34e2f0796deebd35eff4
Adam Sparks <adamhsparks@gmail.com>
2021-12-15 07:15:11 +0800

    Clean up zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,9 @@
 # Borrowed from opencage, https://github.com/ropensci/opencage/blob/main/R/zzz.R
 # We use `<<-` below to modify the package's namespace.
 # It doesn't modify the global environment.
-# We do this to prevent build time dependencies on {memoise} and {ratelimitr},
+# We do this to prevent build time dependencies on {ratelimitr},
 # as recommended in <http://memoise.r-lib.org/reference/memoise.html#details>.
 # Cf. <https://github.com/r-lib/memoise/issues/76> for further details.
-
-# First make sure that the functions are defined at build time
-# .send_query() can be found in internal_functions.R
-.send_query_limited <- .send_query
-.send_query_limited_hourly <- .send_query
 
 # Then modify them at load-time
 # nocov start
