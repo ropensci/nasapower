f303823a196170c8aa3bb914421b0caa619a9729
Adam H. Sparks <adamhsparks@icloud.com>
2023-06-13 18:41:20 +0800

    Serialise cassettes with JSON

--- tests/testthat/setup-nasapower.R
+++ tests/testthat/setup-nasapower.R
@@ -1,3 +1,16 @@
-library("vcr") # *Required* as 'vcr' is set up on loading
-invisible(vcr::vcr_configure(dir = "../fixtures"))
+library("vcr")
+
+vcr_dir <- vcr::vcr_test_path("fixtures")
+vcr::vcr_configure(serialize_with = "json")
 vcr::check_cassette_names()
+
+if (!nzchar(Sys.getenv("GITHUB_PAT"))) {
+  if (dir.exists(vcr_dir)) {
+    # Fake API token to fool our package
+    Sys.setenv("GITHUB_PAT" = "foobar")
+  } else {
+    # If there's no mock files nor API token, impossible to run tests
+    stop("No API key nor cassettes, tests cannot be run.",
+         call. = FALSE)
+  }
+}
