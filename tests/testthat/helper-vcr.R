cca1f528ad96e388fdf5a88bafdf70a85280b149
Adam H. Sparks <adamhsparks@icloud.com>
2023-07-09 12:11:48 +0800

    Update {vcr} configuration

--- tests/testthat/helper-vcr.R
+++ tests/testthat/helper-vcr.R
@@ -1,6 +1,7 @@
-library("vcr")
-
-vcr_dir <- vcr::vcr_test_path("fixtures")
+library("vcr") # *Required* as vcr is set up on loading
+invisible(vcr::vcr_configure(
+  dir = vcr::vcr_test_path("fixtures")
+))
 vcr::vcr_configure(serialize_with = "json")
 vcr::check_cassette_names()
 
