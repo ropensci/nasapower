254f020cb2fda85ff1afddb488f1c25849b5b3fc
Adam H. Sparks <adamhsparks@icloud.com>
2023-08-19 10:59:39 +0800

    Update testing environment for {vcr}

--- tests/testthat/helper-vcr.R
+++ tests/testthat/helper-vcr.R
@@ -1,3 +1,4 @@
+
 library("vcr") # *Required* as vcr is set up on loading
 invisible(vcr::vcr_configure(
   dir = vcr::vcr_test_path("fixtures")
@@ -6,7 +7,7 @@ vcr::vcr_configure(serialize_with = "json")
 vcr::check_cassette_names()
 
 if (!nzchar(Sys.getenv("GITHUB_PAT"))) {
-  if (dir.exists(vcr_dir)) {
+  if (dir.exists(dir)) {
     # Fake API token to fool our package
     Sys.setenv("GITHUB_PAT" = "foobar")
   } else {
