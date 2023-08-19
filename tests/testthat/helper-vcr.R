d48c4aa5e1430c10450dbc12f8b131265b6329ec
Adam H. Sparks <adamhsparks@icloud.com>
2023-08-19 17:35:53 +0800

    try using stripped down helper-vcr.R

--- tests/testthat/helper-vcr.R
+++ tests/testthat/helper-vcr.R
@@ -1,18 +1,6 @@
-
 library("vcr") # *Required* as vcr is set up on loading
 invisible(vcr::vcr_configure(
   dir = vcr::vcr_test_path("fixtures")
 ))
 vcr::vcr_configure(serialize_with = "json")
 vcr::check_cassette_names()
-
-if (!nzchar(Sys.getenv("GITHUB_PAT"))) {
-  if (dir.exists(dir)) {
-    # Fake API token to fool our package
-    Sys.setenv("GITHUB_PAT" = "foobar")
-  } else {
-    # If there's no mock files nor API token, impossible to run tests
-    stop("No API key nor cassettes, tests cannot be run.",
-         call. = FALSE)
-  }
-}
