e404b72cc93086d0e71688ae296197dd3159262c
Adam H. Sparks <adamhsparks@icloud.com>
2024-01-31 20:00:14 +0800

    tidy up tests and add for query_surfaces()

--- tests/testthat/test-query_parameters.R
+++ tests/testthat/test-query_parameters.R
@@ -1,39 +1,48 @@
 
 test_that("query_parameters() returns proper list of info", {
-            skip_on_cran()
-            par_query <- query_parameters(community = "ag",
-                                          pars = "T2M",
-                                          temporal_api = "Daily")
+  skip_if_offline()
+  vcr::use_cassette("query_parameters_comm&temporal_api", {
+    par_query <- query_parameters(community = "ag",
+                                  pars = "T2M",
+                                  temporal_api = "Daily")
 
-            expect_type(par_query, "list")
-            expect_length(par_query, 1)
-            expect_named(par_query, "T2M")
-          })
-
-test_that("query_parameters() returns list of parameter information", {
-  skip_on_cran()
-  par_query <- query_parameters(pars = "T2M")
-  expect_type(par_query, "list")
-  expect_length(par_query, 1)
-  expect_named(par_query, "T2M")
+    expect_type(par_query, "list")
+    expect_length(par_query, 1)
+    expect_named(par_query, "T2M")
+  })
 })
 
+test_that("query_parameters() returns list of parameter information",
+          {
+            skip_if_offline()
+            vcr::use_cassette("query_parameters_all", {
+              par_query <- query_parameters(pars = "T2M")
+              expect_type(par_query, "list")
+              expect_length(par_query, 1)
+              expect_named(par_query, "T2M")
+            })
+          })
+
 test_that("query_parameters() stops if par and community only supplied", {
-  skip_on_cran()
+  skip_if_offline()
   expect_error(query_parameters(pars = "T2M",
                                 community = "AG"))
 })
 
 test_that("query_parameters() stops if par and community only supplied", {
-  skip_on_cran()
+  skip_if_offline()
   expect_error(query_parameters(pars = "T2M",
                                 temporal_api = "daily"))
 })
 
 test_that("query_parameters() stops if metadata is not Boolean", {
-  skip_on_cran()
-  expect_error(query_parameters(pars = "T2M",
-                                community = "ag",
-                                temporal_api = "daily",
-                                metadata = "orange"))
+  skip_if_offline()
+  expect_error(
+    query_parameters(
+      pars = "T2M",
+      community = "ag",
+      temporal_api = "daily",
+      metadata = "orange"
+    )
+  )
 })
--- tests/testthat/test-query_surfaces.R
+++ tests/testthat/test-query_surfaces.R
@@ -1,17 +1,43 @@
 
 test_that("query_surfaces() returns proper list of info", {
-            skip_on_cran()
-            surface_query <- query_surfaces()
+  skip_if_offline()
+  vcr::use_cassette("query_surfaces_all", {
+    surface_query <- query_surfaces()
 
-            expect_type(par_query, "list")
-            expect_length(par_query, 1)
-            expect_named(par_query, "T2M")
-          })
+    expect_type(surface_query, "list")
+    expect_length(surface_query, 17)
+    expect_named(
+      surface_query,
+      c(
+        "vegtype_1",
+        "vegtype_2",
+        "vegtype_3",
+        "vegtype_4",
+        "vegtype_5",
+        "vegtype_6",
+        "vegtype_7",
+        "vegtype_8",
+        "vegtype_9",
+        "vegtype_10",
+        "vegtype_11",
+        "vegtype_12",
+        "vegtype_20",
+        "seaice",
+        "openwater",
+        "airportice",
+        "airportgrass"
+      )
+    )
+  })
+})
 
 test_that("query_surfaces() returns list of parameter information", {
-  skip_on_cran()
-  surface_query <- query_surfaces(pars = "T2M")
-  expect_type(par_query, "list")
-  expect_length(par_query, 1)
-  expect_named(par_query, "T2M")
+  skip_if_offline()
+  vcr::use_cassette("query_surfaces_seaice", {
+    surface_query <- query_surfaces(surface_alias = "seaice")
+    expect_type(surface_query, "list")
+    expect_length(surface_query, 4)
+    expect_named(surface_query,
+                 c("Long_Name", "IGBP_Type", "Veg_Type", "Roughness"))
+  })
 })
