9f8aecd9a405a1537d936eabd8f8a178f09bc9c6
Adam H. Sparks <adam.sparks@curtin.edu.au>
2024-01-31 14:56:31 +0800

    add query_surfaces()

---
+++ R/query_surfaces.R
@@ -0,0 +1,38 @@
+
+#' Query the POWER API for Detailed Information on Wind Type Surfaces
+#'
+#' Queries the \acronym{POWER} \acronym{API} returning detailed information on
+#'   all (or just one) wind elevation surface alias and attribute information.
+#'
+#' @param surface_alias An optional character vector providing a wind surface
+#'   alias available from the \acronym{POWER} \acronym{API}.  All values are
+#'   returned if this value is not provided.
+#'
+#' @examplesIf interactive()
+#'
+#' # fetch all wind surface information
+#' query_surfaces()
+#'
+#' # fetch surface information for `airportgrass`
+#' query_surfaces(surface_alias = "airportgrass")
+#'
+#' @author Adam H. Sparks, \email{adamhsparks@@gmail.com}
+#'
+#' @return A [list] object of information for the requested wind surface(s).
+#'
+#' @export
+
+
+query_surfaces <- function(surface_alias = NULL) {
+
+  power_url <-
+    "https://power.larc.nasa.gov/api/system/manager/surface"
+
+  if (is.null(surface_alias)) {
+    return(jsonlite::fromJSON(txt = power_url))
+  } else {
+    wind_surface <- .match_surface_alias(surface_alias)
+    return(jsonlite::fromJSON(
+      txt = sprintf("%s/%s", power_url, wind_surface)))
+  }
+}
