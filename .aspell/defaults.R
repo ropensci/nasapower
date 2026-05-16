a9195bf097a8b966c49a7df5b95342f6ceb7f1fb
Adam H. Sparks <adamhsparks@icloud.com>
2026-05-16 19:41:47 +0800

    chore: reformat R file using air

--- .aspell/defaults.R
+++ .aspell/defaults.R
@@ -1,5 +1,6 @@
 Rd_files <- vignettes <- R_files <- description <-
-  list(encoding = "UTF-8",
-       language = "en",
-       dictionaries = c("en_stats", "nasapower"))
-
+  list(
+    encoding = "UTF-8",
+    language = "en",
+    dictionaries = c("en_stats", "nasapower")
+  )
